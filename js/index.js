02d2d2c7ea7bace2c0e6c081d7d6a5f7d6524290
Stalin Caiche <scaiche@grupo-link.com>
2019-06-05 11:40:41 -0500

    Validaciones iniciales y si es una cédula o ruc

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 
 function validate_document() {
 
-    $('.alert-success').html('');
-    $('.alert-success').append('Prueba de validación con onclick');
+    var documento = $('#document').val();
 
-    $('.alert-success').removeClass("hide").addClass("show");
+    // instanciamos la clase validate y llamamos el método init
+    var objValidate = new validateDocument( documento );
+    var resValidate = objValidate.init();
+
+    // borramos la información de las cajas de mensaje
+    $('.alert-success').html('');
+    $('.alert-danger').html('');
+
+    // validamos si la respuesta en un error o un success
+    if( resValidate.type == 'error' ){
+        $('.alert-danger').append( resValidate.msn );
+        $('.alert-success').removeClass("show").addClass("hide");
+        $('.alert-danger').removeClass("hide").addClass("show");
+    }else{
+        $('.alert-success').append( resValidate.msn );
+        $('.alert-danger').removeClass("show").addClass("hide");
+        $('.alert-success').removeClass("hide").addClass("show");
+    }
 }
