593c3ed986a6acadbfa2a039f6c64d157be10984
Stalin Caiche <scaiche@grupo-link.com>
2019-06-05 11:04:20 -0500

    Implementando onclick (prueba de mensaje)

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+function validate_document() {
+
+    $('.alert-success').html('');
+    $('.alert-success').append('Prueba de validación con onclick');
+
+    $('.alert-success').removeClass("hide").addClass("show");
+}
